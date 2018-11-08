566babb61a6777e72c6400c555dcf4d2ffbb9e13
John Kamenik <jkamenik@fourv.com>
2018-11-08 08:09:39 -0500

    Adding debug logging and double escaping the '&' for the user.

--- startwork.lua
+++ startwork.lua
@@ -1,5 +1,9 @@
+local logger = hs.logger.new("startwork",'info')
+
 function open(args)
-  hs.execute("open " .. args)
+  local command = "open " .. args
+  logger:i(command)
+  hs.execute(command)
 end
 
 function start_work()
@@ -16,8 +20,8 @@ function start_work()
   -- start("Atom")
   -- start("iTerm")
   
-  -- Jira to CRPI with "DevOps" filter selected.
-  open("https://opaqnetworks.atlassian.net/secure/RapidBoard.jspa?rapidView=35&projectKey=CRPI&view=detail&quickFilter=99")
+  -- Jira to CRPI with "DevOps" filter selected.  Double escape the '\' so that '&' is escaped when sending to the terminal. 
+  open("https://opaqnetworks.atlassian.net/secure/RapidBoard.jspa?rapidView=35\\&quickFilter=99")
 end
 
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "S", start_work)
