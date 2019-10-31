216b6789382ea11be33347835c9beabf9bd8332e
John Kamenik <jkamenik@opaqnetworks.com>
2019-10-31 07:10:12 -0400

    Delay the start of Mail until much later in the day.

--- startwork.lua
+++ startwork.lua
@@ -6,11 +6,18 @@ function open(args)
   hs.execute(command)
 end
 
+-- The following are apps that, if started with everything else cause
+-- distraction
+function start_delayed_apps()
+  local start = hs.application.launchOrFocus
+
+  start(globals.apps.mail)
+end
+
 function start_work()
   local start = hs.application.launchOrFocus
 
   start(globals.apps.messenger.personal)
-  start(globals.apps.mail)
   start(globals.apps.messenger.work)
   start(globals.apps.browser)
   start(globals.apps.todo.app_name)
@@ -23,7 +30,34 @@ function start_work()
   open("https://opaqnetworks.atlassian.net/secure/RapidBoard.jspa?rapidView=49\\&selectedIssue=CRPI-7532\\&quickFilter=134")
 
   -- Jira DSO main board.
-  open("https://opaqnetworks.atlassian.net/secure/RapidBoard.jspa?rapidView=56")
+  -- open("https://opaqnetworks.atlassian.net/secure/RapidBoard.jspa?rapidView=56")
+
+  -- Now do some math, to figure out when to start delayed apps
+  print("--- Delay logic ---")
+  -- local humanTime   = "09:00:00"
+  local humanTime   = "09:00:00"
+  local earliest    = hs.timer.seconds(humanTime)
+  local currentTime = hs.timer.localTime()
+
+  -- Clean any older timers
+  if globals.timers.startWork then
+    print("Stopping existing timer")
+    globals.timers.startWork:stop()
+  end
+
+  if currentTime >= earliest then
+    print("After " .. humanTime)
+    start_delayed_apps()
+  else
+    print("Before earliest")
+    local timer = hs.timer.doAt(earliest, start_delayed_apps)
+    globals.timers.startWork = timer
+    print(timer)
+
+    local message = string.format("Delayed app will start after %s (approx. %ds)",humanTime, (earliest-currentTime))
+    print(message)
+    hs.alert.show(message)
+  end
 end
 
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "S", start_work)
