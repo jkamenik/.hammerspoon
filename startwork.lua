49ed5e05adb6f5b8cc8399727cee6015f5ac62c4
John Kamenik <jkamenik@opaqnetworks.com>
2019-10-31 10:16:15 -0400

    Running start work now creates a menu item for the delayed apps.

--- startwork.lua
+++ startwork.lua
@@ -1,5 +1,8 @@
 local logger = hs.logger.new("startwork",'info')
 
+local humanTime = "11:00:00"
+local earliest  = hs.timer.seconds(humanTime)
+
 function open(args)
   local command = "open " .. args
   logger:i(command)
@@ -9,9 +12,34 @@ end
 -- The following are apps that, if started with everything else cause
 -- distraction
 function start_delayed_apps()
+  print("-- starting delayed apps --")
   local start = hs.application.launchOrFocus
 
+  -- Clean any older timers
+  if globals.timers.startWork then
+    print("Stopping existing timer")
+    globals.timers.startWork:stop()
+  end
+
   start(globals.apps.mail)
+
+  remove_delay_menu()
+end
+
+local delayedAppsMenu = {
+  title = "Start Delayed Apps Now (will start at "..humanTime..")",
+  fn    = start_delayed_apps
+}
+function remove_delay_menu()
+  printf("Delay menu item %s", delayedAppsMenu)
+  for k, v in pairs(menuItems) do
+    printf("%s - %s", k, v)
+    if v == delayedAppsMenu then
+      printf("Removing %d menu item",k)
+      table.remove(menuItems,k)
+    end
+  end
+  menu:setMenu(menuItems)
 end
 
 function start_work()
@@ -36,9 +64,6 @@ function start_work()
 
   -- Now do some math, to figure out when to start delayed apps
   print("--- Delay logic ---")
-  -- local humanTime   = "09:00:00"
-  local humanTime   = "09:00:00"
-  local earliest    = hs.timer.seconds(humanTime)
   local currentTime = hs.timer.localTime()
 
   -- Clean any older timers
@@ -55,6 +80,10 @@ function start_work()
     local timer = hs.timer.doAt(earliest, start_delayed_apps)
     globals.timers.startWork = timer
     print(timer)
+
+    print("Adding menu Items")
+    table.insert(menuItems, delayedAppsMenu)
+    menu:setMenu(menuItems)
 
     local message = string.format("Delayed app will start after %s (approx. %ds)",humanTime, (earliest-currentTime))
     print(message)
