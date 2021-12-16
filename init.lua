4cd616e818c55b0f8fcff7092bea1bc347688b94
John Kamenik <john.kamenik@fireeye.com>
2021-12-16 07:46:12 -0500

    Switch to using Spoons to reload the config.

--- init.lua
+++ init.lua
@@ -1,15 +1,10 @@
-function reloadConfig(files)
-   doReload = false
-   for _,file in pairs(files) do
-      if file:sub(-4) == ".lua" then
-         doReload = true
-      end
-   end
-   if doReload then
-      hs.reload()
-   end
-end
-hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+local rc = hs.loadSpoon("ReloadConfiguration")
+rc:start()
+
+-- local pp = hs.loadSpoon("PrettyPrint")
+-- x = { "this", "that"}
+-- print(x)
+-- print(pp.stringify(x))
 
 -- Create a menu bar
 menu       = hs.menubar.new()
